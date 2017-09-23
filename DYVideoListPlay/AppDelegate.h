8f04b4bf71aa1997e38648b07d94ceb194ed5393
阳眼的熊 <237505990@qq.com>
2017-09-23 09:12:38 +0800

    Initial Commit

---
+++ DYVideoListPlay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DYVideoListPlay
+//
+//  Created by huyangyang on 2017/9/23.
+//  Copyright © 2017年 huyangyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DYVideoListPlay/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DYVideoListPlay
+//
+//  Created by huyangyang on 2017/9/23.
+//  Copyright © 2017年 huyangyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
